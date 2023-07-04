9f88b26f9b1202d3228d9b3431c6f9e66c5a3b18
Ante <varto1488@gmail.com>
2023-07-04 14:54:09 +0100

    add all of it

---
+++ README.md
@@ -0,0 +1,9 @@
+## Usage
+
+To take advantage of this code you will need to add your own Firebase client project config and Firebase Admin SDK service account as explained in the course setup.
+
+```
+git clone <this-repo>
+npm i
+npm run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { userData } from "$lib/firebase";
+</script>
+
+<svelte:head>
+  <title>Legend - a place for links</title>
+  <meta
+    name="description"
+    content="Demo project for the Full SvelteKit Course by Fireship"
+  />
+</svelte:head>
+
+<main class="flex w-full min-h-screen">
+  <div class="hero bg-base-200">
+    <div class="hero-content text-center">
+      <div class="w-full">
+        <h1 class="text-7xl font-bold">Legend😎</h1>
+        <p class="py-6 text-2xl">
+          One simple link to share your entire portfolio
+        </p>
+        {#if $userData?.username}
+          <a href="/{$userData.username}/edit" class="btn btn-primary"
+            >Edit Profile</a
+          >
+        {:else}
+          <a href="/login" class="btn btn-primary">Get Started</a>
+          <a href="/fireship" class="btn btn-secondary">Example Profile</a>
+        {/if}
+      </div>
+    </div>
+  </div>
+</main>
